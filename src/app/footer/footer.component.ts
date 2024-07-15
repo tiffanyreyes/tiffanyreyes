6037116b8b7a796bf4abb7cb0e23e2dde27da906
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2024-07-14 23:35:06 -0400

    update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
